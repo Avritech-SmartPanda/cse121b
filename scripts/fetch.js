1929013f2ddd1a5e3d599c12eb5bef49107a652e
Daphne Avril <daphneavril@outlook.com>
2023-10-23 21:03:28 +0200

    w05 task

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,5 +1,6 @@
 // fetch.js
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
 
 async function getPokemon(url) {
@@ -12,6 +13,18 @@ async function getPokemon(url) {
     doStuff(data);
   }
 }
+
+async function getPokemonList(urlList) {
+  const response = await fetch(urlList);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuffList(data);
+  }
+}
+
 function doStuff(data) {
   results = data;
   var output = document.querySelector("#output");
@@ -20,5 +33,41 @@ function doStuff(data) {
   output.innerHTML = html;
   console.log("first: ", results);
 }
+
+function doStuffList(data) {
+  pokeList = data.results;
+  pokeList = sortPokemon(pokeList);
+  var outputList = document.querySelector("#outputList");
+  pokeList.forEach((element) => {
+    const html = `<li>${element.name}</li>`;
+    outputList.innerHTML += html;
+  });
+
+  console.log("list: ", results);
+}
+
+
+
+
+function compare(a, b) {
+  if (a.name > b.name) {
+    // sort b before a
+    return 1;
+  } else if (a.name < b.name) {
+    // a and b different but unchanged (already in the correct order)
+    return -1;
+  } else return 0; // a and b are equal
+}
+
+function sortPokemon(list) {
+  let sortedList = list.sort(compare);
+  return sortedList;
+}
+
+
+
+
+
 getPokemon(url);
+getPokemonList(urlList);
 console.log("second: ", results);
--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,20 +1,93 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-
+const templesElement = document.querySelector("#temples");
+let templeList = [];
 
 /* async displayTemples Function */
-
-
-
+const displayTemples = (temples) => {
+  templesElement.innerHTML = "";
+  temples.forEach((temple) => {
+    const article = document.createElement("article");
+    const h3 = document.createElement("h3");
+    h3.textContent = temple.templeName;
+    const img = document.createElement("img");
+    img.setAttribute("src", temple.imageUrl);
+    img.setAttribute("alt", temple.location);
+    article.appendChild(h3);
+    article.appendChild(img);
+    templesElement.appendChild(article);
+  });
+};
 
 /* async getTemples Function using fetch()*/
+const getTemples = async () => {
+  const response = await fetch(
+    "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json"
+  );
+  if (response.ok) {
+    templeList = await response.json();
+    displayTemples(templeList);
+  }
 
+  console.log(templeList);
+};
 
 /* reset Function */
-
+const reset = function () {
+  templesElement.innerHTML = "";
+};
 
 /* sortBy Function */
+// Sort the list of temples by the selected value of the HTML select element with an ID of sortBy. The preset selections include temples located in the state of Utah, those outside the state of Utah, and temples built before 1950 on the list.
+
+// Declare a function expression named sortBy.
+// The function should accept a argument in a parameter named temples.
+// In this function, first call the reset function to clear the output.
+// Define a variable named filter that obtains the value of the HTML element with the ID of sortBy (The pull-down menu).
+// Use a switch statement that uses the filter value as the selector responding to four (4) cases.
+// For each case, call the displayTemples function using an filter statement that filters the temples parameter for the four options provided.
+// "utah": filter for temples where the location contains "Utah" as a string.
+// "nonutah": filter for temples where the location does not contain "Utah" as a string.
+// "older": filter for temples where the dedicated date is before 1950. (compare versus new Date(1950, 0, 1)).
+// "all": no filter. Just use temples as the argument.
+
+const sortBy = function (temples) {
+  reset();
+  let filter = document.querySelector("#sortBy").value;
+  switch (filter) {
+    case "utah":
+      displayTemples(
+        temples.filter((temple) => temple.location.includes("Utah"))
+      );
+      break;
+    case "nonutah":
+      displayTemples(
+        temples.filter((temple) => !temple.location.includes("Utah"))
+      );
+      break;
+    case "older":
+      displayTemples(
+        temples.filter(
+          (temple) => new Date(temple.dedication) < new Date(1950, 0, 1)
+        )
+      );
+      break;
+    case "all":
+      displayTemples(temples);
+      break;
+  }
+};
+
+// Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function and sends a arrow function result with the templeList as the argument.
+
+document.querySelector("#sortBy").addEventListener("change", () => {
+  sortBy(templeList);
+});
+
+
+
+
 
 
 
